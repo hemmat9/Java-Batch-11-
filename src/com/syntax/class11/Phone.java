489f255150a4a532475cc04eb6b8738e173bcb7a
“hemmat9” <farhad.hemmat9@gmail.com>
2021-11-03 21:25:29 -0700

    Second commit

--- src/com/syntax/class11/Phone.java
+++ src/com/syntax/class11/Phone.java
@@ -2,60 +2,48 @@ package com.syntax.class11;
 
 public class Phone {
 //Create a Class “Phone”. Create 3 Objects of it: iPhone, Android, Nokia with specific  attributes and behaviors.
-	
-	
-	String name;
-	String brand;
-	String colors;
-	int weight;
-	String display;
-	String made;
-	
-	void music() {
-		System.out.println(name + " can play music and vidoes with "+ display);
-	}
-	
-	void internet() {
-		System.out.println(name + " is enabled to be connected to the internet");
-	}
-	
-	void calls() {
-		System.out.println(name + " enables you to make calls and text and it just weights "+ weight+"grs");
-	}
-	
-	public static void main(String[] args) {
-		Phone iPhone=new Phone();
-		iPhone.name="iPhone";
-		iPhone.made="United States";
-		iPhone.display="HD Display";
-		iPhone.weight=200;
-		iPhone.colors="Black, white, red, gold";
-		System.out.println("Name: "+iPhone.name);
-		System.out.println("Made in: "+iPhone.made);
-		System.out.println(iPhone.name+" has "+iPhone.display+" Display");
-		System.out.println(iPhone.name+ " is available in colors "+ iPhone.colors);
-		iPhone.music();
-		iPhone.calls();
-		iPhone.internet();
+		String name;
+		String brand;
+		String colors;
+		int weight;
+		String display;
+		String made;
+
+		public static void main(String[] args) {
+		Phone iPhone = new Phone();
+		iPhone.name = "iPhone";
+		iPhone.made = "United States";
+		iPhone.display = "HD Display";
+		iPhone.weight = 180;
+		iPhone.colors = "Black, white, red, gold";
+		System.out.println("Name: " + iPhone.name);
+		System.out.println("Made in: " + iPhone.made);
+		System.out.println(iPhone.name + " has " + iPhone.display + " Display");
+		System.out.println(iPhone.name + " is available in colors " + iPhone.colors);
+
 		System.out.println("========================================================");
 		Phone Nokia = new Phone();
-		Nokia.name="Nokia";
-		Nokia.made="Finland";
-		Nokia.display="IPS LCD";
-		Nokia.weight=160;
-		Nokia.brand="Nokia";}
-		
-		
-		void music1() {
-			System.out.println(name + " can play music and vidoes with "+ display);
-		}
-		
-		void internet2() {
-			System.out.println(name + " is enabled to be connected to the internet");
-		}
-		
-		void calls3() {
-			System.out.println(name + " enables you to make calls and text and it just weights "+ weight+"grs");
-		}
+		Nokia.name = "Nokia";
+		Nokia.made = "Finland";
+		Nokia.display = "IPS LCD";
+		Nokia.weight = 160;
+		Nokia.brand = "Nokia";
+		Nokia.colors = "Black, white, red";
+		System.out.println("Name: " + Nokia.name);
+		System.out.println("Made in: " + Nokia.made);
+		System.out.println(Nokia.name + " has " + Nokia.display + " Display");
+		System.out.println(Nokia.name + " is available in colors " + Nokia.colors);
+		System.out.println("==================================================");
+		Phone Andriod = new Phone();
+		Andriod.name = "Andriod Phone";
+		Andriod.made = "Vietnam";
+		Andriod.display = "Full HD and Quad HD";
+		Andriod.weight = 170;
+		Andriod.brand = "Samsung";
+		Andriod.colors = "Black, white, gold";
+		System.out.println("Name: " + Andriod.name);
+		System.out.println("Made in: " + Andriod.made);
+		System.out.println(Andriod.name + " has " + Andriod.display + " Display");
+		System.out.println(Andriod.name + " is available in colors " + Andriod.colors);
 	}
-
+}
